8779e788696c16c92684b89498157f9e274954e9
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-24 21:19:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ const inputs = document.querySelectorAll('.controls input');
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('video');
+   const video = document.getElementById('video');
     const playPauseButton = document.getElementById('play-pause');
     const rewindButton = document.getElementById('rewind');
     const forwardButton = document.getElementById('forward');
-	
+    const volumeInput = document.getElementById('volume');
+    const playbackSpeedInput = document.getElementById('playbackSpeed');
+    const progressFilled = document.getElementById('progress__filled');
+    const speedBar = document.getElementById('speed-bar');
+
+    video.addEventListener('timeupdate', () => {
+        const percent = (video.currentTime / video.duration) * 100;
+        progressFilled.style.width = `${percent}%`;
+    });
+
+    playPauseButton.addEventListener('click', () => {
+        if (video.paused) {
+            video.play();
+            playPauseButton.textContent = '❚ ❚'; 
+        } else {
+            video.pause();
+            playPauseButton.textContent = '►'; 
+        }
+    });
     rewindButton.addEventListener('click', () => {
         video.currentTime -= 10;
     });
@@ -21,4 +39,14 @@ document.addEventListener('DOMContentLoaded', () => {
     forwardButton.addEventListener('click', () => {
         video.currentTime += 25;
     });
+
+    volumeInput.addEventListener('input', () => {
+        video.volume = volumeInput.value / 100;
+    });
+
+    playbackSpeedInput.addEventListener('input', () => {
+        video.playbackRate = playbackSpeedInput.value;
+        speedBar.textContent = `${playbackSpeedInput.value}×`;
+    });
+	
 });
